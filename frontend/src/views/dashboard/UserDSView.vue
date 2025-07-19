f51c6651bedcc6c8dbabca820d54f6da986986a2
DemuraAIdev <cyberaioff@gmail.com>
2025-07-19 17:23:40 +0700

    Nice detail

--- frontend/src/views/dashboard/UserDSView.vue
+++ frontend/src/views/dashboard/UserDSView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import HeaderDash from "@/components/HeaderDash.vue";
+import { useAuthStore } from "@/stores/auth";
+const user = useAuthStore();
 </script>
 
 <template>
   <HeaderDash />
-  <main>Dashboard</main>
+  <main>You are : {{ user.role[0] }}</main>
 </template>
